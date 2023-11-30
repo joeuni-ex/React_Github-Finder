c80bc9be27bca4bfdddd382af38dda1cc7f89f4b
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-30 10:11:34 +0900

    Navbar 컴포넌트 생성

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,40 @@
+import { FaGithub } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+const Navbar = ({ title }) => {
+  return (
+    <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
+      <div className="container mx-auto">
+        <div className="flex-none px-2 mx-2">
+          <FaGithub className="inline pr-2 text-3xl" />
+          <Link to="/" className="text-lg font-bold align-middle">
+            {title}
+          </Link>
+        </div>
+
+        <div className="flex-1 px-2 mx-2">
+          <div className="flex justify-end">
+            <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
+              HOME
+            </Link>
+            <Link to="/about" className="btn btn-ghost btn-sm rounded-btn">
+              ABOUT
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+//컴포넌트의 프롭스 타입을 설정
+Navbar.propTypes = {
+  //타이틀은 문자열
+  title: PropTypes.string,
+};
+//기본값
+Navbar.defaultProps = {
+  title: "Github Finder",
+};
+
+export default Navbar;
