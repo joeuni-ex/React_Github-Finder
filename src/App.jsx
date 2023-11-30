454424cdf6dbadc428e35254f7d1aa57847f4a0e
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-30 09:28:00 +0900

    데이지UI 설치 후 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <>앱</>;
+  return (
+    <div className="bg-purple-500">
+      <h1 className="text-xl">헬로우 월드!</h1>
+      <button className="btn">클릭!</button>
+    </div>
+  );
 }
 
 export default App;
