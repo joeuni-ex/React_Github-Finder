d1bda0f04fb944f9f4005f9b571cf28279a1a0f6
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-30 14:35:03 +0900

    state-> reducer로 변경

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext();
 
@@ -6,8 +7,15 @@ const GITHUB_URL = import.meta.env.VITE_GITHUB_URL;
 const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true); //시작 시 깃허브에서 데이터를 가져옴(작업중 true)
+  // const [users, setUsers] = useState([]);
+  //const [loading, setLoading] = useState(true); //시작 시 깃허브에서 데이터를 가져옴(작업중 true)
+
+  const initialState = {
+    users: [],
+    loading: true,
+  };
+  //리듀서 사용
+  const [state, dispatch] = useReducer(githubReducer, initialState);
 
   const fetchUsers = async () => {
     //onsole.log(`${import.meta.env.VITE_GITHUB_TOKEN}/users`);
@@ -18,15 +26,20 @@ export const GithubProvider = ({ children }) => {
     });
     const data = await response.json();
 
-    setUsers(data);
-    setLoading(false); // 데이터 로딩 완료(false)
+    // setUsers(data);
+    // setLoading(false); // 데이터 로딩 완료(false)
+
+    dispatch({
+      type: "GET_USERS",
+      payload: data,
+    });
   };
 
   return (
     <GithubContext.Provider
       value={{
-        users,
-        loading,
+        users: state.users,
+        loading: state.loading,
         fetchUsers,
       }}
     >
