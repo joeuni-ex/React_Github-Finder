cf7deb4e16acf882bce8c31c9a49ccc8aeebd934
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-30 11:50:48 +0900

    로딩 스테이트 추가 및 스피너 추가

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
+import Spinner from "../layout/Spinner";
 
 function UserResults() {
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true); //시작 시 깃허브에서 데이터를 가져옴(작업중 true)
 
   useEffect(() => {
     fetchUsers();
@@ -17,14 +19,19 @@ function UserResults() {
     const data = await response.json();
 
     setUsers(data);
+    setLoading(false); // 데이터 로딩 완료(false)
   };
-  return (
-    <div className="grid grid-cols-8 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-      {users.map((user) => (
-        <h3>{user.login}</h3>
-      ))}
-    </div>
-  );
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-8 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => (
+          <h3 key={user.id}>{user.login}</h3>
+        ))}
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
 }
 
 export default UserResults;
