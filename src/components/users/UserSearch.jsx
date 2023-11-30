b91ec478c2db87a7c457a3f5d0a8f0c12b2d2cbd
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-30 15:14:17 +0900

    UserSearch기능추가

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import GithubContext from "../../context/github/GithubContext";
 
 const UserSearch = () => {
+  const [text, setText] = useState(""); //검색어
+  const { users } = useContext(GithubContext);
+
+  //검색어 저장
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  //submit 시
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (text === "") {
+      alert("내용을 입력해주세요");
+    } else {
+      //유저찾기
+      setText("");
+    }
+  };
   return (
     <div
       className="grid grid-cols-1 xl:grid-cols-2 
           lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8"
     >
       <div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-control">
             <div className="relative">
               <input
+                onChange={handleChange}
+                value={text}
                 type="text"
                 className="w-full pr-40 bg-gray-200 input input-lg text-black"
                 placeholder="Search"
@@ -26,7 +48,9 @@ const UserSearch = () => {
         </form>
       </div>
       <div>
-        <button className="btn btn-ghost btn-lg">Clear</button>
+        {users.length > 0 && (
+          <button className="btn btn-ghost btn-lg">Clear</button>
+        )}
       </div>
     </div>
   );
