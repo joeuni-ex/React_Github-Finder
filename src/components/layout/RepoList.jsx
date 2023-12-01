5ea83952f15fabbc3926337e6472b043421cad4a
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-01 11:36:22 +0900

    RepoList컨텍스트에서 Repo 하나씩 출력

--- src/components/layout/RepoList.jsx
+++ src/components/layout/RepoList.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-const RepoList = () => {
-  return <div>RepoList</div>;
+const RepoList = ({ repos }) => {
+  // console.log(repos);
+  return (
+    <div className="rounded-lg shadow-lg card bg-base-100">
+      <div className="card-body">
+        <h2 className="text-3xl my-4 font-bold card-title">
+          최근 리포 10개 표시
+        </h2>
+        {repos.map((repo) => (
+          <h3 key={repo.id}>{repo.name}</h3>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default RepoList;
