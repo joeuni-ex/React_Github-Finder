91fd89cd66a15596c9233d4cfe35cff991ca6cbc
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-01 10:03:58 +0900

    User 컴포넌트 생성 후 라우트적용

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import NotFound from "./components/pages/NotFound";
 import Alert from "./components/layout/Alert";
+import User from "./components/pages/User";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/user/:login" element={<User />} />
             <Route path="/notfound" element={<NotFound />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
